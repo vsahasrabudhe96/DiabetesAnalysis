bd03020101a13b1bef5d89698b6eadf4d6e1810f
Varun Sahasrabudhe <59940037+vsahasrabudhe96@users.noreply.github.com>
2020-06-13 21:30:04 -0400

    Update model.py

--- model.py
+++ model.py
@@ -17,11 +17,6 @@ X = df.drop(labels='Outcome',axis=1)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-'''x_train,x_test,y_train,y_test = train_test_split(xcopy,ycopy,test_size=0.2,random_state = 32)
-sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)'''
-
 #APPLYING FEW CLASSIFICATION TECHNIQUES
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
@@ -38,22 +33,6 @@ dic = {'LR': LogisticRegression(max_iter=200),
        }
 k = list(dic.keys())
 
-'''accuracy={}
-recall={}
-f1 = {}
-accuracy_test={}
-for i in k:
-    model = dic[i]
-    model.fit(x_train,y_train)
-    y_pred = model.predict(x_train)
-    y_t_pred = model.predict(x_test)
-    ac = accuracy_score(y_true=y_train,y_pred=y_pred)
-    accuracy[i] = ac
-    recall[i] = recall_score(y_true=y_train,y_pred=y_pred)
-    f1[i] = f1_score(y_true=y_train,y_pred=y_pred)
-    accuracy_test[i] = accuracy_score(y_true=y_test,y_pred=y_t_pred)
-
-'''
 X_tr,X_te,y_tr,y_te = train_test_split(X,y,test_size=0.2,random_state=32)
 X_tr = np.array(X_tr)
 X_te = np.array(X_te)
@@ -77,6 +56,6 @@ for it in k:
 opt_model = dic['SVM']
 pickle.dump(opt_model,open('model.pkl','wb'))
 
-#zz = pickle.load(open('model.pkl','rb'))
-#rint(zz.predict([[0,35,72,35,100,32.5,0.5,50]]))
+zz = pickle.load(open('model.pkl','rb'))
+print(zz.predict([[0,35,72,35,100,32.5,0.5,50]]))
 
